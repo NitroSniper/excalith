e80fcce8df794fcb354e867f0ee64325c89fda69
NitroSniper <mahiemiah35@gmail.com>
2023-12-23 23:03:23 +0000

    changed default config

--- startpage.config.js
+++ startpage.config.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-	username: "Excalith",
+	username: "nitro",
 	theme: {
 		backgroundColor: "#121317",
 		windowColor: "#1e212b",
@@ -29,13 +29,13 @@ const defaultConfig = {
 	},
 	prompt: {
 		ctrlC: true,
-		placeholder: "command...",
+		placeholder: "",
 		placeholderColor: "gray",
-		userColor: "green",
+		userColor: "red",
 		atColor: "gray",
-		hostColor: "magenta",
+		hostColor: "cyan",
 		promptColor: "magenta",
-		promptSymbol: "❯",
+		promptSymbol: "➜",
 		caretColor: "green",
 		selectionBg: "yellow",
 		selectionFg: "black"
@@ -59,23 +59,13 @@ const defaultConfig = {
 	urlLaunch: {
 		target: "_self",
 		defaultColor: "white",
-		hoverColor: "violet"
+		hoverColor: "orange"
 	},
 	search: {
 		default: "https://google.com/search?q=",
 		target: "_self",
 		shortcutRegex: "([A-Za-z0-9]+) (.*)",
 		shortcuts: [
-			{
-				alias: "g",
-				name: "Google Search",
-				url: "https://google.com/search?q={}"
-			},
-			{
-				alias: "d",
-				name: "DuckDuckGo Search",
-				url: "https://duckduckgo.com/?q={}"
-			},
 			{
 				alias: "b",
 				name: "Brave Search",
@@ -87,20 +77,15 @@ const defaultConfig = {
 				url: "https://github.com/search?q={}"
 			},
 			{
-				alias: "s",
-				name: "Stack Overflow Search",
-				url: "https://stackoverflow.com/search?q={}"
-			},
-			{
 				alias: "r",
 				name: "Subreddit Search",
 				url: "https://reddit.com/r/{}"
 			},
 			{
-				alias: "w",
-				name: "Wikipedia Search",
-				url: "https://en.wikipedia.org/wiki/{}"
-			}
+				alias: "repo",
+				name: "Repository Search",
+				url: "https://github.com/NitroSniper/{}"
+			},
 		]
 	},
 	sections: {
@@ -110,161 +95,126 @@ const defaultConfig = {
 				color: "green",
 				align: "left",
 				links: [
-					{
-						name: "Portfolio",
-						url: "https://cancellek.com",
-						icon: "mdi:web"
-					},
-					{
-						name: "Keybase",
-						url: "https://keybase.io/",
-						icon: "fa-brands:keybase"
-					},
-					{
-						name: "GPT",
-						url: "https://chat.openai.com/",
-						icon: "simple-icons:openai"
-					},
-					{
-						name: "OCI",
-						url: "https://www.oracle.com/cloud/",
-						icon: "simple-icons:oracle"
-					}
 				]
 			},
 			{
 				title: "Dev",
-				color: "magenta",
+				color: "cyan",
 				align: "left",
 				links: [
 					{
 						name: "GitHub",
-						url: "https://github.com",
+						url: "https://github.com/NitroSniper",
 						icon: "mdi:github"
 					},
 					{
-						name: "GitLab",
-						url: "https://gitlab.com",
-						icon: "ph:gitlab-logo-simple-fill"
+						name: "GPT",
+						url: "https://chat.openai.com",
+						icon: "simple-icons:openai"
 					},
 					{
-						name: "Dev.to",
-						url: "https://dev.to",
-						icon: "material-symbols:logo-dev"
+						name: "Advent Of Code",
+						url: "https://adventofcode.com",
+						icon: "simple-icons:adventofcode"
 					},
-					{
-						name: "Stack Overflow",
-						url: "https://stackoverflow.com/",
-						icon: "mdi:stack-overflow"
-					}
 				]
 			},
 			{
-				title: "Social",
-				color: "violet",
+				title: "Services",
+				color: "magenta",
 				align: "left",
 				links: [
-					{
-						name: "Twitter",
-						url: "https://twitter.com",
-						icon: "mdi:twitter"
-					},
-					{
-						name: "Mastodon",
-						url: "https://mastodon.social/",
-						icon: "ri:mastodon-fill"
-					},
 					{
 						name: "Reddit",
 						url: "https://reddit.com",
 						icon: "mdi:reddit"
 					},
 					{
-						name: "Polywork",
-						url: "https://polywork.com",
-						icon: "simple-icons:polywork"
-					}
+						name: "Youtube",
+						url: "https://youtube.com",
+						icon: "mdi:youtube"
+					},
+					{
+						name: "Gmail",
+						url: "https://mail.google.com",
+						icon: "mdi:gmail"
+					},
+					{
+						name: "Spotify",
+						url: "https://open.spotify.com",
+						icon: "mdi:spotify"
+					},
 				]
 			},
 			{
-				title: "Gaming",
-				color: "cyan",
+				title: "Valorant",
+				color: "red",
 				align: "left",
 				links: [
 					{
-						name: "Polygon",
-						url: "https://polygon.com",
-						icon: "uil:polygon"
+						name: "Valo Tracker",
+						url: "https://tracker.gg/valorant/profile/riot/NitroSniper%23NERD/overview",
+						icon: "healthicons:magnifying-glass"
 					},
 					{
-						name: "IGN",
-						url: "https://ign.com",
-						icon: "mdi:currency-sign"
+						name: "VCT",
+						url: "https://vct.gg",
+						icon: "streamline:esports"
 					},
 					{
-						name: "RPS",
-						url: "https://rockpapershotgun.com/",
-						icon: "ph:toilet-paper-bold"
+						name: "VLR",
+						url: "https://vlr.gg",
+						icon: "iconoir:stats-up-square"
 					},
 					{
-						name: "80lv",
-						url: "https://80.lv/",
-						icon: "tabler:hand-rock"
-					}
+						name: "TMV Twitch",
+						url: "https://www.twitch.tv/thinkingmansvalo",
+						icon: "mdi:twitch"
+					},
 				]
 			},
 			{
-				title: "Science",
-				color: "blue",
-				align: "left",
-				links: [
-					{
-						name: "PopSci",
-						url: "https://popsci.com/",
-						icon: "material-symbols:science"
-					},
-					{
-						name: "Space",
-						url: "fa6-solid:user-astronaut",
-						icon: "mdi:reddit"
-					},
-					{
-						name: "NASA",
-						url: "https://blogs.nasa.gov/",
-						icon: "simple-icons:nasa"
-					},
-					{
-						name: "ESA",
-						url: "https://blogs.esa.int/",
-						icon: "mdi:black-mesa"
-					}
-				]
-			},
-			{
-				title: "Tech",
+				title: "University",
 				color: "yellow",
 				align: "left",
 				links: [
 					{
-						name: "TechCrunch",
-						url: "https://techcrunch.com/",
-						icon: "game-icons:techno-heart"
+						name: "Blackboard",
+						url: "https://learn.uea.ac.uk/",
+						icon: "icon-park-outline:blackboard"
 					},
 					{
-						name: "Verge",
-						url: "https://www.theverge.com/",
-						icon: "arcticons:verge"
+						name: "Outlook",
+						url: "fa6-solid:user-astronaut",
+						icon: "file-icons:microsoft-outlook"
 					},
 					{
-						name: "It's Foss",
-						url: "https://itsfoss.com/",
-						icon: "ri:mastodon-fill"
+						name: "Timetabler",
+						url: "https://blogs.nasa.gov/",
+						icon: "mdi:timetable"
 					},
 					{
-						name: "9To5 Linux",
-						url: "https://9to5linux.com/",
-						icon: "uil:linux"
-					}
+						name: "SU Events",
+						url: "https://www.ueasu.org/tickets/",
+						icon: "mdi:events"
+					},
+				]
+			},
+			{
+				title: "Tech",
+				color: "blue",
+				align: "left",
+				links: [
+					{
+						name: "Monkeytype",
+						url: "https://monkeytype.com",
+						icon: "simple-icons:monkeytype"
+					},
+					{
+						name: "Lichess",
+						url: "https://lichess.org",
+						icon: "simple-icons:lichess"
+					},
 				]
 			}
 		]
